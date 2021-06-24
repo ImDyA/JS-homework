8e9416f7443f680bd3bab1c41539d4ada99525ae
ImDyA <vasurman13@gmail.com>
2021-06-24 14:58:28 +0300

    home work leson-1

---
+++ script.js
@@ -0,0 +1,10 @@
+let userAge = 24;
+let userName = "Vasia";
+let userSurName = "Petkin";
+let fullUser = userAge + " " + userName + " " + userSurName;
+console.log(fullUser);
+let newUser;
+let shoppingList;
+let userOnline;
+let salaryUser;
+let cursorCoordinatesUser;
